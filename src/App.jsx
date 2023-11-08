5904cd2a44b99c2aef67a359ea754027bc254f19
Jasmine <jsmnratliff@gmail.com>
2023-11-07 17:23:36 -0800

    yo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import './styles.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
-import './Styles.css';
 
 function App() {
   return (
