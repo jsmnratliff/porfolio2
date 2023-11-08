93e1f71d89eaf7bef21d54e7d4ba6354bfa8328e
Jasmine <jsmnratliff@gmail.com>
2023-11-07 17:17:11 -0800

    ajg

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
+import './Styles.css';
+
 function App() {
   return (
     <div>
